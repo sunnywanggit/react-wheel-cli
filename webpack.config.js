a00bf13388865e2def9ae014484f60ea8fd947cd
ergouwang <330035385@qq.com>
2020-07-02 17:53:34 +0800

    index.tsx 可以打包成 index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const  path = require('path')
 module.exports = {
+    mode: 'development',
     //程序入口
-    entry: {index: './lib/index.tsx'},
+    entry: {sunnyReactUI: './lib/index.tsx'},
+    //程序输出
+    output: {
+        path: path.resolve(__dirname,'dist/lib'),
+        library: 'sunnyReactUI',
+        //输出格式
+        libraryTarget: 'umd'
+    },
     module: {
         //配置解析 jsx 方式
         rules: [
